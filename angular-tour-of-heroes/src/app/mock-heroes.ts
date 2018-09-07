553535865d6b96bffd071898b199a1b07e01f82c
Miki <tornyosi.m@gmail.com>
2018-09-07 17:46:55 +0200

    https://github.com/Weakens/angular-tour-of-heroes.git

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Accurato', logo: 'xXx' },
+    { id: 12, name: 'Bombasto', logo: '<<o>>' },
+    { id: 13, name: 'Speedo', logo: '>>>' },
+    { id: 14, name: 'Celeritas', logo: '' },
+    { id: 15, name: 'Vulcano', logo: '/|\\' },
+    { id: 16, name: 'RubberMan', logo: '~o~' },
+    { id: 17, name: 'Teleporto', logo: '.:\'' },
+    { id: 18, name: 'Dr IQ', logo: '[3.14]' },
+    { id: 19, name: 'Magmato', logo: '(/\)' },
+    { id: 20, name: 'Tornado', logo: '(|()|)' }
+];
